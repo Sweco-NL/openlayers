6628f9e4a2ada3adff32d21c5ef51347ff72accc
Enzo Claveau <65344540+sweco-nlecla@users.noreply.github.com>
2022-05-02 13:46:50 +0200

    Fix styling issue with curve polygons (#5)
    * Fix styling issue with curve polygons
    
    * Add different stroke width to curve polygon example

--- src/ol/render/canvas/CurveBuilder.js
+++ src/ol/render/canvas/CurveBuilder.js
@@ -76,6 +76,7 @@ class CanvasCurveBuilder extends CanvasBuilder {
     }
     this.setFillStrokeStyles();
     this.beginGeometry(curvePolygonGeometry, feature);
+    this.instructions.push(beginPathInstruction);
     this.appendCurvePolygonInstructions(curvePolygonGeometry);
     this.endGeometry(feature);
   }
@@ -270,7 +271,6 @@ class CanvasCurveBuilder extends CanvasBuilder {
     }
     state.lastStroke = 0;
     super.applyStroke(state);
-    this.instructions.push(beginPathInstruction);
   }
 }
 
